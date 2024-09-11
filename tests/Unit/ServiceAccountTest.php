2d4bce7dc19c41dbb4c3328b80ac9baaba1c1f17
Kal Marsh <kal.marsh@gov.bc.ca>
2024-09-11 09:53:25 -0700

    moves env injection to bc

---
+++ tests/Unit/ServiceAccountTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ServiceAccountTest extends TestCase
+{
+    public function test_fetch_data(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_fetch_tables(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_fetch_columns(): void
+    {
+        $this->assertTrue(true);
+    }
+}
